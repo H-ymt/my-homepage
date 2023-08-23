e1d35fccb1fba6cae91f3d53d4fe124bc319eb59
H-ymt <107012772+H-ymt@users.noreply.github.com>
2023-08-23 17:27:39 +0900

    style:font-sizeを変更する

--- components/SectionTitle/index.tsx
+++ components/SectionTitle/index.tsx
@@ -7,7 +7,7 @@ export default function SectionTitle({ children, eng }: Props) {
   return (
     <div className="text-center">
       <h2 className="inline-flex flex-col pb-6 text-3xl lg:text-4xl">
-        <span className="font-light text-sm text-left">{eng}</span>
+        <span className="font-light text-xs lg:text-sm text-left">{eng}</span>
         {children}
       </h2>
     </div>
