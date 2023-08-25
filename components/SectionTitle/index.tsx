422677e3bd5653fa79c1dca04dda4082c19856f7
H-ymt <y.handai1272@gmail.com>
2023-08-25 15:31:24 +0900

    style:font-weightを太くする

--- components/SectionTitle/index.tsx
+++ components/SectionTitle/index.tsx
@@ -7,10 +7,10 @@ export default function SectionTitle({ children, eng }: Props) {
   return (
     <div className="text-center">
       <h2 className="inline-flex flex-col pb-6 lg:pb-14 text-3xl lg:text-4xl">
-        <span className="font-light text-xs lg:text-sm text-center">
+        {children}
+        <span className="font-medium text-xs lg:text-sm text-center text-muted">
           {eng}
         </span>
-        {children}
       </h2>
     </div>
   );
