1b959351f608f58201d53b637fbb11a736a3c735
H-ymt <y.handai1272@gmail.com>
2023-08-30 17:21:02 +0900

    add:日付のフォーマット関数を実装する

---
+++ components/ConvertDate/index.tsx
@@ -0,0 +1,10 @@
+import dayjs from 'dayjs';
+
+type Props = {
+  convertDate: string | number | Date;
+};
+
+export default function ConvertDate({ convertDate }: Props) {
+  const publishedAt = dayjs(convertDate).format('YYYY/MM/DD/');
+  return <time dateTime={convertDate.toString()}>{publishedAt}</time>;
+}
