65d61a280dd514e97880473e72a765a4399948ae
H-ymt <y.handai1272@gmail.com>
2023-08-31 18:01:54 +0900

    add:contactページの実装

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,8 +1,18 @@
+'use client';
+
+import Form from '@/components/Form';
+import Sheet from '@/components/Sheet';
+
 export default function Page() {
   return (
     <>
-      <h1>Contact</h1>
-      <p>お問い合せはこちらから</p>
+      <Sheet>
+        <section>
+          <h1>Contact</h1>
+          <p>お問い合せはこちらから</p>
+          <Form />
+        </section>
+      </Sheet>
     </>
   );
 }
