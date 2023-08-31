36b42d452ff839c0bec121da53b291ffc0e86fef
H-ymt <y.handai1272@gmail.com>
2023-08-31 17:08:40 +0900

    add:404ページの実装

---
+++ app/not-found.tsx
@@ -0,0 +1,17 @@
+'use client';
+
+import Link from 'next/link';
+
+export default function NotFound() {
+  return (
+    <div>
+      <h1 className="py-40 text-2xl">404 - Page Not Found</h1>
+
+      <div className="flex justify-center">
+        <Link href="/" className="font-bold">
+          Homeへ戻る&ensp;🏃
+        </Link>
+      </div>
+    </div>
+  );
+}
