df9869abcbe0928dab0a7a8d6796d548ef00451c
H-ymt <y.handai1272@gmail.com>
2023-09-12 18:38:54 +0900

    style:カードの横幅を均等にする

--- components/BlogList/index.tsx
+++ components/BlogList/index.tsx
@@ -24,7 +24,7 @@ export default async function BlogList() {
             <Link
               href={`https://zenn.dev/${post.path}`}
               target="blank"
-              className="inline-flex flex-col items-center justify-center gap-4 min-h-[220px] max-w-[320px] p-6 border-2 bg-orange-200 dark:bg-blue-200/30 border-none rounded-3xl hover:scale-[1.02] duration-200 transition-all"
+              className="inline-flex flex-col items-center justify-center gap-4 w-[300px] min-h-[220px] max-w-[320px] p-6 border-2 bg-orange-200 dark:bg-blue-200/30 border-none rounded-3xl hover:scale-[1.02] duration-200 transition-all"
             >
               <span className="text-6xl">{post.emoji}</span>
               <p className="text-left font-medium line-clamp-2 overflow-hidden break-all">
