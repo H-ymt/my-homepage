5e96cc2338ed9567941b4c1c4cb043870c5c2ade
H-ymt <y.handai1272@gmail.com>
2023-09-13 22:34:18 +0900

    update: ページ遷移アニメーションを追加する

---
+++ app/template.tsx
@@ -0,0 +1,27 @@
+'use client';
+
+import { AnimatePresence, motion } from 'framer-motion';
+import { fadeUp } from './utils/motion';
+import { usePathname } from 'next/navigation';
+
+type Props = {
+  children: React.ReactNode;
+};
+
+export default function RootTemplate({ children }: Props) {
+  const pathname = usePathname();
+
+  return (
+    <AnimatePresence mode="wait">
+      <motion.div
+        variants={fadeUp}
+        initial="hidden"
+        animate="visible"
+        exit="hidden"
+        key={pathname}
+      >
+        {children}
+      </motion.div>
+    </AnimatePresence>
+  );
+}
---
+++ app/utils/motion.ts
@@ -0,0 +1,16 @@
+export const fadeUp = {
+  hidden: {
+    opacity: 0,
+    y: '45px',
+    transition: {
+      duration: 0.5,
+    },
+  },
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+    },
+  },
+};
