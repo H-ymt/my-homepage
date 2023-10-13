cbaab90b92a1bdfefe741fc2d6880d9667c6bc65
H-ymt <y.handai1272@gmail.com>
2023-10-14 00:33:34 +0900

    add: emoji

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -5,7 +5,7 @@ import Link from 'next/link';
 export default function NotFound() {
   return (
     <div>
-      <h1 className="py-40 text-2xl">404 - Page Not Found</h1>
+      <h1 className="py-40 text-2xl">404 - Page Not Found 🔍</h1>
 
       <div className="flex justify-center">
         <Link href="/" className="font-bold">
