80c32e481a06a3a9dd9dc58416728f8f658cd2b1
H-ymt <y.handai1272@gmail.com>
2023-10-16 01:02:44 +0900

    add: skillsセクションのデザインを変更する

--- app/page.tsx
+++ app/page.tsx
@@ -5,6 +5,7 @@ import Link from 'next/link';
 import Sheet from '@/components/Sheet';
 import SectionTitle from '@/components/SectionTitle';
 import * as Dialog from '@radix-ui/react-dialog';
+import * as Tabs from '@radix-ui/react-tabs';
 import { BiSolidFileHtml } from 'react-icons/bi';
 import { BiSolidFileCss } from 'react-icons/bi';
 import { BiLogoJavascript } from 'react-icons/bi';
@@ -46,155 +47,125 @@ export default function Home() {
 
             <div className="py-8">
               <h3 className="text-center">Markup</h3>
-              <div className="flex justify-center gap-6 md:gap-10 mt-8">
-                <Dialog.Root>
-                  <Dialog.Trigger asChild>
-                    <button type="button" className="flex items-center gap-1">
-                      <BiSolidFileHtml size="22px" />
-                      HTML
-                      <PiArrowSquareInLight size="16px" />
-                    </button>
-                  </Dialog.Trigger>
-                  <Dialog.Portal>
-                    <Dialog.Overlay className="z-30 bg-gray-900/30 dark:bg-gray-900/80 data-[state=open]:animate-overlayShow fixed inset-0" />
-                    <Dialog.Content className="z-40 dark:text-primaryDark data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
-                      <Dialog.Title>
-                        HTML
-                        <span className="block mt-1 text-xs text-muted font-light">
-                          実務経験:2年
-                        </span>
-                      </Dialog.Title>
-                      <Dialog.Description className="mt-2 text-sm">
-                        Webアクセシビリティ（a11y）を考慮したコーディングやEJSの知識があります。
-                      </Dialog.Description>
-                    </Dialog.Content>
-                  </Dialog.Portal>
-                </Dialog.Root>
 
-                <Dialog.Root>
-                  <Dialog.Trigger asChild>
-                    <button type="button" className="flex items-center gap-1">
-                      <BiSolidFileCss size="22px" />
-                      CSS
-                      <PiArrowSquareInLight size="16px" />
-                    </button>
-                  </Dialog.Trigger>
-                  <Dialog.Portal>
-                    <Dialog.Overlay className="z-30 bg-gray-900/30 dark:bg-gray-900/80 data-[state=open]:animate-overlayShow fixed inset-0" />
-                    <Dialog.Content className="z-40 dark:text-primaryDark data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
-                      <Dialog.Title>
-                        CSS
-                        <span className="block mt-1 text-xs text-muted font-light">
-                          実務経験：2年
-                        </span>
-                      </Dialog.Title>
-                      <Dialog.Description className="mt-2 text-sm">
-                        Sass（SCSS）を利用したスタイリング、BEMやFLOCSSなどを用いた保守性・拡張性の高いCSS設計の知識があります。
-                        <br />
-                        最近はAstroやNext.jsなどを用いたコンポーネントベースのWeb制作にハマっているのでTailwind
-                        CSSを使用する機会も増えました。
-                      </Dialog.Description>
-                    </Dialog.Content>
-                  </Dialog.Portal>
-                </Dialog.Root>
-
-                <Dialog.Root>
-                  <Dialog.Trigger asChild>
-                    <button type="button" className="flex items-center gap-1">
-                      <BiLogoJavascript size="22px" />
-                      JavaScript
-                      <PiArrowSquareInLight size="16px" />
-                    </button>
-                  </Dialog.Trigger>
-                  <Dialog.Portal>
-                    <Dialog.Overlay className="z-30 bg-gray-900/30 dark:bg-gray-900/80 data-[state=open]:animate-overlayShow fixed inset-0" />
-                    <Dialog.Content className="z-40 dark:text-primaryDark data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
-                      <Dialog.Title>
-                        JavaScript
-                        <span className="block mt-1 text-xs text-muted font-light">
-                          実務経験：2年
-                        </span>
-                      </Dialog.Title>
-                      <Dialog.Description className="mt-2 text-sm">
-                        ES6以降のJavaScriptを利用したWebサイト制作の経験、また、レガシーなWebサイトの保守・運用業務でjQueryを利用した経験もあります。
-                      </Dialog.Description>
-                    </Dialog.Content>
-                  </Dialog.Portal>
-                </Dialog.Root>
-              </div>
+              <Tabs.Root className="flex flex-col mt-8" defaultValue="tab1">
+                <Tabs.List
+                  className="shrink-0 flex border border-gray-600 rounded-t-md"
+                  aria-label="My skill sets"
+                >
+                  <Tabs.Trigger
+                    className="bg-transparent px-5 h-[45px] flex-1 flex items-center justify-center gap-2 text-base leading-none select-none first:rounded-tl-md last:rounded-tr-md hover:text-orange-400 dark:hover-text-blue-400 data-[state=active]:text-orange-400 dark:data-[state=active]:text-blue-400 data-[state=active]:font-medium data-[state=active]:shadow-[inset_0_-1px_0_0,0_2px_0_0] cursor-pointer"
+                    value="tab1"
+                  >
+                    <BiSolidFileHtml size="16px" />
+                    HTML
+                  </Tabs.Trigger>
+                  <Tabs.Trigger
+                    className="bg-transparent px-5 h-[45px] flex-1 flex items-center justify-center gap-2 text-base leading-none select-none first:rounded-tl-md last:rounded-tr-md hover:text-orange-400 dark:hover-text-blue-400 data-[state=active]:text-orange-400 dark:data-[state=active]:text-blue-400 data-[state=active]:font-medium data-[state=active]:shadow-[inset_0_-1px_0_0,0_2px_0_0] cursor-pointer"
+                    value="tab2"
+                  >
+                    <BiSolidFileCss size="16px" />
+                    CSS
+                  </Tabs.Trigger>
+                  <Tabs.Trigger
+                    className="bg-transparent px-5 h-[45px] flex-1 flex items-center justify-center gap-2 text-base leading-none select-none first:rounded-tl-md last:rounded-tr-md hover:text-orange-400 dark:hover-text-blue-400 data-[state=active]:text-orange-400 dark:data-[state=active]:text-blue-400 data-[state=active]:font-medium data-[state=active]:shadow-[inset_0_-1px_0_0,0_2px_0_0] cursor-pointer"
+                    value="tab3"
+                  >
+                    <BiLogoJavascript size="16px" />
+                    JavaScript
+                  </Tabs.Trigger>
+                </Tabs.List>
+                <Tabs.Content
+                  className="border border-gray-600 border-t-0 text-base grow p-5 bg-transparent rounded-b-md"
+                  value="tab1"
+                >
+                  <span className="block mt-1 pb-2 text-sm text-muted font-light">
+                    実務経験:2年
+                  </span>
+                  Webアクセシビリティを考慮したコーディング、EJSの知識があります。
+                </Tabs.Content>
+                <Tabs.Content
+                  className="border border-gray-600 border-t-0 text-base grow p-5 bg-transparent rounded-b-md"
+                  value="tab2"
+                >
+                  <span className="block mt-1 pb-2 text-sm text-muted font-light">
+                    実務経験:2年
+                  </span>
+                  Sass（SCSS）を利用したスタイリング、BEMやFLOCSSなどを用いた保守性・拡張性の高いCSS設計の知識があります。
+                  <br />
+                  CSSフレームワークはTailwind CSSが使えます。
+                </Tabs.Content>
+                <Tabs.Content
+                  className="border border-gray-600 border-t-0 text-base grow p-5 bg-transparent rounded-b-md"
+                  value="tab3"
+                >
+                  <span className="block mt-1 pb-2 text-sm text-muted font-light">
+                    実務経験:2年
+                  </span>
+                  ES6以降のJavaScriptを利用したWebサイト制作の経験、また、レガシーなWebサイトの保守・運用業務でjQueryを利用した経験もあります。
+                </Tabs.Content>
+              </Tabs.Root>
             </div>
 
             <div className="py-8">
               <h3 className="text-center">Frameworks</h3>
 
-              <div className="flex justify-center gap-6 md:gap-10 mt-8">
-                <Dialog.Root>
-                  <Dialog.Trigger asChild>
-                    <button type="button" className="flex items-center gap-1">
-                      <SiNextdotjs size="22px" />
-                      React(Next.js)
-                      <PiArrowSquareInLight size="16px" />
-                    </button>
-                  </Dialog.Trigger>
-                  <Dialog.Portal>
-                    <Dialog.Overlay className="z-30 bg-gray-900/30 dark:bg-gray-900/80 data-[state=open]:animate-overlayShow fixed inset-0" />
-                    <Dialog.Content className="z-40 dark:text-primaryDark data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
-                      <Dialog.Title>
-                        React(Next.js)
-                        <span className="block mt-1 text-xs text-muted font-light">
-                          実務経験:なし（独学のみ）
-                        </span>
-                      </Dialog.Title>
-                      <Dialog.Description className="mt-2 text-sm">
-                        microCMSなどのヘッドレスCMSをつかったWebサイトの制作ができます。
-                        <br />
-                        WordPressでつくられた自社サイトのコーポレートサイトを、趣味で勝手にリニューアルしてみたり、作りたいものを気ままに作っています。
-                      </Dialog.Description>
-                    </Dialog.Content>
-                  </Dialog.Portal>
-                </Dialog.Root>
-
-                <Dialog.Root>
-                  <Dialog.Trigger asChild>
-                    <button type="button" className="flex items-center gap-1">
-                      <SiAstro size="22px" />
-                      Astro
-                      <PiArrowSquareInLight size="16px" />
-                    </button>
-                  </Dialog.Trigger>
-                  <Dialog.Portal>
-                    <Dialog.Overlay className="z-30 bg-gray-900/30 dark:bg-gray-900/80 data-[state=open]:animate-overlayShow fixed inset-0" />
-                    <Dialog.Content className="z-40 dark:text-primaryDark data-[state=open]:animate-contentShow fixed top-[50%] left-[50%] max-h-[85vh] w-[90vw] max-w-[450px] translate-x-[-50%] translate-y-[-50%] rounded-[6px] bg-white p-[25px] shadow-[hsl(206_22%_7%_/_35%)_0px_10px_38px_-10px,_hsl(206_22%_7%_/_20%)_0px_10px_20px_-15px] focus:outline-none">
-                      <Dialog.Title>
-                        Astro
-                        <span className="block mt-1 text-xs text-muted font-light">
-                          実務経験:なし（独学のみ）
-                        </span>
-                      </Dialog.Title>
-                      <Dialog.Description className="mt-2 text-sm">
-                        microCMS、WordPressをヘッドレスCMSとして利用したWebサイトの制作ができます。
-                      </Dialog.Description>
-                    </Dialog.Content>
-                  </Dialog.Portal>
-                </Dialog.Root>
-              </div>
+              <Tabs.Root className="flex flex-col mt-8" defaultValue="tab1">
+                <Tabs.List
+                  className="shrink-0 flex border border-gray-600 rounded-t-md"
+                  aria-label="My skill sets"
+                >
+                  <Tabs.Trigger
+                    className="bg-transparent px-5 h-[45px] flex-1 flex items-center justify-center gap-2 text-base leading-none select-none first:rounded-tl-md last:rounded-tr-md hover:text-orange-400 dark:hover-text-blue-400 data-[state=active]:text-orange-400 dark:data-[state=active]:text-blue-400 data-[state=active]:font-medium data-[state=active]:shadow-[inset_0_-1px_0_0,0_2px_0_0] cursor-pointer"
+                    value="tab1"
+                  >
+                    <SiNextdotjs size="16px" />
+                    React(Next.js)
+                  </Tabs.Trigger>
+                  <Tabs.Trigger
+                    className="bg-transparent px-5 h-[45px] flex-1 flex items-center justify-center gap-2 text-base leading-none select-none first:rounded-tl-md last:rounded-tr-md hover:text-orange-400 dark:hover-text-blue-400 data-[state=active]:text-orange-400 dark:data-[state=active]:text-blue-400 data-[state=active]:font-medium data-[state=active]:shadow-[inset_0_-1px_0_0,0_2px_0_0] cursor-pointer"
+                    value="tab2"
+                  >
+                    <SiAstro size="16px" />
+                    Astro
+                  </Tabs.Trigger>
+                </Tabs.List>
+                <Tabs.Content
+                  className="border border-gray-600 border-t-0 text-base grow p-5 bg-transparent rounded-b-md"
+                  value="tab1"
+                >
+                  <span className="block mt-1 pb-2 text-sm text-muted font-light">
+                    実務経験:なし（独学のみ）
+                  </span>
+                  microCMSなどのヘッドレスCMSをつかったJamstackなWebサイトの制作ができます。
+                </Tabs.Content>
+                <Tabs.Content
+                  className="border border-gray-600 border-t-0 text-base grow p-5 bg-transparent rounded-b-md"
+                  value="tab2"
+                >
+                  <span className="block mt-1 pb-2 text-sm text-muted font-light">
+                    実務経験:なし（独学のみ）
+                  </span>
+                  microCMS、WordPressをヘッドレスCMSとして利用したWebサイトの制作ができます。
+                </Tabs.Content>
+              </Tabs.Root>
             </div>
 
             <div className="py-8">
               <h3 className="text-center">Others</h3>
 
               <div className="grid sm:grid-cols-2 sm:grid-rows-2 gap-6 mt-8">
-                <div className="flex flex-col gap-1 p-4 border-2 border-primaryDark dark:border-white rounded-lg">
+                <div className="flex flex-col gap-1 p-4 border border-gray-600 rounded-lg">
                   <h4 className="font-semibold">バージョン管理</h4>
                   <p className="text-base">Git/GitHub</p>
                 </div>
-                <div className="flex flex-col gap-1 p-4 border-2 border-primaryDark dark:border-white rounded-lg">
+                <div className="flex flex-col gap-1 p-4 border border-gray-600 rounded-lg">
                   <h4 className="font-semibold">CMS</h4>
                   <p className="text-base">
                     microCMS,&ensp;Wordpress,&ensp;Newt,&ensp;Kuroco
                   </p>
                 </div>
-                <div className="flex flex-col gap-1 p-4 border-2 border-primaryDark dark:border-white rounded-lg">
+                <div className="flex flex-col gap-1 p-4 border border-gray-600 rounded-lg">
                   <h4 className="font-semibold">Cloud&ensp;Service</h4>
                   <p className="text-base">
                     Vercel,&ensp;Netlify,&ensp;Cloudflare&ensp;
