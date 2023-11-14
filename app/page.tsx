7219238605f9c45b3418b63045c555ca5eaf0b16
H-ymt <y.handai1272@gmail.com>
2023-11-14 22:26:12 +0900

    test

--- app/page.tsx
+++ app/page.tsx
@@ -13,7 +13,7 @@ export default function Home() {
   return (
     <>
       <div className="flex items-center pb-24 pt-20 md:pt-32">
-        <div className="mx-auto  flex max-w-xl flex-col items-center justify-center px-5">
+        <div className="mx-auto max-w-xl flex-col items-center justify-center px-5 flex">
           <h1 className="text-4xl tracking-wider md:text-[40px]">My Homepage</h1>
 
           <div className="flex-col items-center gap-8 flex">
