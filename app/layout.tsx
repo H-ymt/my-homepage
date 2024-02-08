999197bd9ce7d823326f745a0ed106678d208827
H-ymt <y.handai1272@gmail.com>
2024-02-08 11:11:19 +0900

    chore(site title): update sitetitle#25
    update #25

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,30 +1,30 @@
-import Header from '@/app/_components/Header'
-import { Montserrat } from 'next/font/google'
-import './globals.css'
-import { Providers } from './providers'
+import Header from "@/app/_components/Header"
+import { Montserrat } from "next/font/google"
+import "./globals.css"
+import { Providers } from "./providers"
 
-const montserrat = Montserrat({ subsets: ['latin'] })
+const montserrat = Montserrat({ subsets: ["latin"] })
 
 export const metadata = {
-  metadataBase: new URL('https://h-ymt.com/'),
+  metadataBase: new URL("https://h-ymt.com/"),
   title: {
-    template: "%s - Yamato's Portfolio",
-    default: "Yamato's Portfolio",
+    template: "%s - Portfolio",
+    default: "Portfolio",
   },
   description:
-    'Next.jsとTailwind CSSを使用したホームページです。コンテンツ管理にmicroCMSを使用したJamstack構成で、ホスティングにはCloudflare Pagesを使用しています。',
+    "Next.jsとTailwind CSSを使用したホームページです。コンテンツ管理にmicroCMSを使用したJamstack構成で、ホスティングにはCloudflare Pagesを使用しています。",
   twitter: {
-    title: "Yamato's Portfolio",
+    title: "Portfolio",
     description:
-      'Next.jsとTailwind CSSを使用したホームページです。コンテンツ管理にmicroCMSを使用したJamstack構成で、ホスティングにはCloudflare Pagesを使用しています。',
-    card: 'summary',
+      "Next.jsとTailwind CSSを使用したホームページです。コンテンツ管理にmicroCMSを使用したJamstack構成で、ホスティングにはCloudflare Pagesを使用しています。",
+    card: "summary",
   },
-  viewport: 'width=device-width, initial-scale=1, viewport-fit=cover',
+  viewport: "width=device-width, initial-scale=1, viewport-fit=cover",
 }
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
-    <html lang='ja' suppressHydrationWarning>
+    <html lang="ja" suppressHydrationWarning>
       <body
         className={`${montserrat.className} bg-background bg-gradient-to-r from-background via-background to-transparent text-primary`}
       >
