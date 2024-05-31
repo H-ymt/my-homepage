636d0486de497fb84f5e5d470e35de6eaa95488d
半代大和 <y.handai1272@gmail.com>
2024-05-31 14:47:43 +0900

    feat(zenn): Add Zenn articles

--- app/components/BlogList/index.tsx
+++ app/components/BlogList/index.tsx
@@ -1,4 +1,8 @@
+'use client'
+
+import { fadeUpStaggerAnimation } from '@/lib/motion'
 import { ZennArticle } from '@/lib/zenn'
+import { motion } from 'framer-motion'
 import BlogListItem from '../BlogListItem'
 
 type Props = {
@@ -13,10 +17,15 @@ export default function BlogList({ posts }: Props) {
   }
 
   return (
-    <>
+    <motion.div
+      variants={fadeUpStaggerAnimation(0, 0, 0, 1, 0.5, 0.1)}
+      initial="hidden"
+      animate="visible"
+      className="grid sm:grid-cols-2 gap-y-7 gap-x-8"
+    >
       {posts.map(post => (
         <BlogListItem key={post.id} posts={post} />
       ))}
-    </>
+    </motion.div>
   )
 }
