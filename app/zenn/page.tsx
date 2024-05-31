22649b3516223e4f981e1afc2e07a214d43f2d19
半代大和 <y.handai1272@gmail.com>
2024-05-31 18:22:03 +0900

    fix(zenn): Run with the edge runtime

--- app/zenn/page.tsx
+++ app/zenn/page.tsx
@@ -3,6 +3,8 @@ import { FaExternalLinkAlt } from 'react-icons/fa'
 import BlogList from '../components/BlogList'
 import ScrapList from '../components/ScrapList'
 
+export const runtime = 'edge'
+
 export default async function ZennPage() {
   const data = await getZennArticles()
 
